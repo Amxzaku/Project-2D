d1553c496ee1d5acfb448620583f70a21e2ff787
Dan <zexion313@gmail.com>
2024-06-16 22:59:01 +0800

    Added Attack button!
    Attack button

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("jump")		
-	
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
@@ -31,6 +31,7 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = false
 	elif direction == -1:
 		$AnimatedSprite2D.flip_h = true	
-
+		
+		
 	move_and_slide()
 
