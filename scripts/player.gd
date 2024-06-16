c5f6d2815f479f8a9b24aab72c0613ffbda0557e
Dan <zexion313@gmail.com>
2024-06-16 22:25:30 +0800

    Update

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,15 +15,14 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("jump")		
-		
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 			
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.play("run")	
-		
+		$AnimatedSprite2D.play("run")			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
