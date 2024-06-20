e3df41edc5a7a6a6ec41cd7212ac47711cf281d2
Amxzaku <j.roz09171997@gmail.com>
2024-06-21 00:08:42 +0800

    Platforms

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,9 @@ const JUMP_VELOCITY = -250.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+
 func _physics_process(delta):
 	if is_on_floor():
 		$AnimatedSprite2D.play("idle")
@@ -14,24 +17,32 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("jump")		
+		$AnimatedSprite2D.play("jump")
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
-			
+	
+	
+# Rotation
+	if direction == 1:
+		animated_sprite.flip_h = false
+
+	elif direction == -1:
+		animated_sprite.flip_h = true
+
+
+	#Play Animation
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.play("run")			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-		# Rotation
-	if direction == 1:
-		$AnimatedSprite2D.flip_h = false
-	elif direction == -1:
-		$AnimatedSprite2D.flip_h = true	
-		
-		
+
 	move_and_slide()
 
