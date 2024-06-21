8233fe43205a9b333fdd85ec6c5d8ed9d00f0666
Amxzaku <j.roz09171997@gmail.com>
2024-06-21 23:50:19 +0800

    Player Animation 2.0
    Run = (UPDATED)
    Jump = (UPDATED)
    Idle = (UPDATED)

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,34 +9,37 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	if is_on_floor():
-		$AnimatedSprite2D.play("idle")
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite2D.play("jump")
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	
-	
-# Rotation
-	if direction == 1:
-		animated_sprite.flip_h = false
 
-	elif direction == -1:
+	# Get the Input direction -1, 0, 1
+	var direction = Input.get_axis("left", "right")
+
+
+	#Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
 		animated_sprite.flip_h = true
 
 
+
+
+
 	#Play Animation
-	if direction == 0:
-		animated_sprite.play("idle")
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
 	else:
-		animated_sprite.play("run")
+		animated_sprite.play("jump")
 	
 	
 	if direction:
